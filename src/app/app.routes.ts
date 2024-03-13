a4e498b515b401b03765aeb27044adc9aa738603
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-03-13 21:38:20 +0530

    [Priyanshu] mod: modified routes, where added new route and changed Keep route to KeepApp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
 import { KeepAppComponent } from './component/keep-app/keep-app.component';
+import { ReviewLoginAppComponent } from './component/review-login-app/review-login-app.component';
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent },
   {path: 'register', component: RegisterComponent},
-  {path: 'keep',component: KeepAppComponent}
+  {path: 'KeepApp',component: KeepAppComponent},
+  {path: "ReviewLogin", component: ReviewLoginAppComponent}
 ];
