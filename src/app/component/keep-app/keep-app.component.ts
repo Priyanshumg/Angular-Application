6b5bb626364a63305b18ddfab206427e7307aa6a
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-03-13 19:39:06 +0530

    [Priyanshu] Add: Added typescript element component for googlekeep app

---
+++ src/app/component/keep-app/keep-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-keep-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './keep-app.component.html',
+  styleUrl: './keep-app.component.scss'
+})
+export class KeepAppComponent {
+
+}
