3169150939706b4940a845a8646797fdeb4b3bf5
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-03-13 19:37:32 +0530

    [Priyanshu] Add: Added routes to googlekeep app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
+import { KeepAppComponent } from './component/keep-app/keep-app.component';
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent },
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'keep',component: KeepAppComponent}
 ];
